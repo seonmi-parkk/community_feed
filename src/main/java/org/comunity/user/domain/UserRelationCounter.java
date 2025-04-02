72aa5fb0d3e54fc6b043617f65fe5636eb6c445c
parkhg09 <parkhg09@gmail.com>
2025-04-02 20:59:49 +0900

    User와 Follwing 기능 초안 (User,UserRelationCounter, UserInfo 클래스)

---
+++ src/main/java/org/comunity/user/domain/UserRelationCounter.java
@@ -0,0 +1,21 @@
+package org.comunity.user.domain;
+
+public class UserRelationCounter {
+
+    private int count;
+
+    public UserRelationCounter() {
+        this.count = 0;
+    }
+
+    public void increase() {
+        this.count++;
+    }
+
+    public void decrease() {
+        if(count <= 0) {
+            return;
+        }
+        this.count--;
+    }
+}
